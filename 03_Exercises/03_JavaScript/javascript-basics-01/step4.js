e7672e1b3ec8ac3bc17a67f6958ec7ebfb3df53d
shant-k <shantk650@gmail.com>
2022-12-07 13:32:17 +0200

    finished step 4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step4.js
@@ -0,0 +1,5 @@
+var name1 = prompt("What's your name?");
+var surname1 = prompt("What's your surname?");
+var city1 = prompt("What's your city?");
+
+alert("hello"+' ' +name1+' '+surname1+'\n'+"From"+' '+city1)
