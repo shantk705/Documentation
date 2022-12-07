b44ef1614e457bd581e86883690923da29f9d757
shant-k <shantk650@gmail.com>
2022-12-07 20:00:56 +0200

    finished step 7

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step7.js
@@ -0,0 +1,18 @@
+document.getElementById('validate').addEventListener('click', function(){
+    const shoeSize = document.getElementById('shoe_size').value
+    const year= document.getElementById('year').value
+const multi = shoeSize*2;
+const add = multi+5;
+const multi2 = add *50;
+const sub = multi2-year;
+const add2 = sub +1766;
+
+
+    const result = add2;
+    alert(result)
+
+
+
+
+
+})
