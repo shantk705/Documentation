ec1dc086aa2dc976a937b4fe2ba52022ee6112b9
shant-k <shantk650@gmail.com>
2022-12-07 22:21:05 +0200

    finished step1

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 1/step1.js
@@ -0,0 +1,10 @@
+function setNewImage() 
+{
+    document.getElementById("image1") .src="images/image1_2.jpg";
+}
+function setOldImage()
+{
+    document.getElementById("image1") .src="images/image1.jpg";
+}
+
+
