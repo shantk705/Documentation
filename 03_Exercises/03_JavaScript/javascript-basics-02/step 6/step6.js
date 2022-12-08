3e1eb135bac3063ed816182a5d1dae6ac58946c3
shant-k <shantk650@gmail.com>
2022-12-08 16:19:27 +0200

    finished step 6

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 6/step6.js
@@ -0,0 +1,39 @@
+function setNewImage(x) 
+{
+    if (x==1){
+    document.querySelector("#image1") .src="images/image1_2.jpg";
+}
+if (x==2){
+    document.querySelector("#image2") .src="images/image2_2.jpg";
+}
+if (x==3){
+    document.querySelector("#image3") .src="images/image3_2.jpg";
+}
+if (x==4){
+    document.querySelector("#image4") .src="images/image4_2.jpg";
+}
+if (x==5){
+    document.querySelector("#image5") .src="images/image5_2.jpg";
+}
+
+}
+function setOldImage(x) 
+{
+    if (x==1){
+    document.querySelector("#image1") .src="images/image1.jpg";
+}
+if (x==2){
+    document.querySelector("#image2") .src="images/image2.jpg";
+}
+if (x==3){
+    document.querySelector("#image3") .src="images/image3.jpg";
+}
+if (x==4){
+    document.querySelector("#image4") .src="images/image4.jpg";
+}
+if (x==5){
+    document.querySelector("#image5") .src="images/image5.jpg";
+}
+
+}
+
