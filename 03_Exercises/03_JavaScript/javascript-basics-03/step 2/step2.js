bfab629dc9b513335fc9c168ae85cb7645cb6945
shant-k <shantk650@gmail.com>
2022-12-08 18:39:06 +0200

    finished step 2

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 2/step2.js
@@ -0,0 +1,8 @@
+function myFunction1(x) {
+    if (x==1){
+        document.getElementById("texte").style.visibility = "visible" ;
+    }
+    if (x==2){
+        document.getElementById("texte").style.visibility = "hidden" ;
+    }
+}
