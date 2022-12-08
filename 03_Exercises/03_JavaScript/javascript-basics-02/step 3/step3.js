2e84ce525d3d045eb57ee034f36f0825c9ef95a6
shant-k <shantk650@gmail.com>
2022-12-08 11:36:06 +0200

    finished step 2 and 3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 3/step3.js
@@ -0,0 +1,10 @@
+
+const myInput = document.getElementById("name")
+myInput.addEventListener("change", () =>{
+    document.getElementById("div1").style.visibility = "visible";
+})
+
+document.getElementById("div1").style.visibility = "hidden";
+
+    
+
