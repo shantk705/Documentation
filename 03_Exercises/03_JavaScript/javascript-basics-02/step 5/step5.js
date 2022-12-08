d9e81ac89d09439373bb8e1633df63b0b0029af4
shant-k <shantk650@gmail.com>
2022-12-08 16:14:16 +0200

    finished step 5

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 5/step5.js
@@ -0,0 +1,23 @@
+function setNewImage(x) 
+{
+    if (x==1){
+    document.querySelector("#image1") .src="images/image1_2.jpg";
+}
+if (x==2){
+    document.querySelector("#image2") .src="images/image2_2.jpg";
+}
+if (x==3){
+    document.querySelector("#image3") .src="images/image3_2.jpg";
+}
+if (x==4){
+    document.querySelector("#image4") .src="images/image4_2.jpg";
+}
+if (x==5){
+    document.querySelector("#image5") .src="images/image5_2.jpg";
+}
+
+}
+
+
+
+
