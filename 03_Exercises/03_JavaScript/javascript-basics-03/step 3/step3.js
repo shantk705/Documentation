dcdaeaacbc482927195b449d0156b942f516d28d
shant-k <shantk650@gmail.com>
2022-12-08 18:50:05 +0200

    finished step 3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 3/step3.js
@@ -0,0 +1,11 @@
+function changeColor(x) {
+    if (x==1){
+        document.getElementById('text').style.color="green";
+    }
+    if (x==2){
+        document.getElementById('text').style.color="red";
+    }
+    if (x==3){
+        document.getElementById('text').style.color="blue";
+    }
+}
