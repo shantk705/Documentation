ced01c251ba276510303dc4270196006edba9618
shant-k <shantk650@gmail.com>
2022-12-09 22:20:11 +0200

    finished basic queries

---
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,38 @@
+((Basic Queries))
+1-SELECT DISTINCT name FROM students;
+
+
+2-SELECT DISTINCT name FROM students
+WHERE Age=30;
+
+
+3-
+SELECT DISTINCT name FROM students
+WHERE Gender='F' and Age=30
+ ;
+
+
+4-SELECT DISTINCT Points FROM students
+WHERE name like 'alex'
+ ;
+
+
+5-INSERT INTO students
+VALUES ('7',' shant', '21','M','0');
+
+
+6-UPDATE students
+SET Points='450'
+WHERE name='Basma';
+
+7- UPDATE students
+SET Points='100'
+WHERE name='Alex';
+
+8- updated answers.sh with answers
+
+9- commited to git !
+
+
+
+
